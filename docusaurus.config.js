7db9c9ea483e09d2f8ea88e5ef4dd1c6165a8451
Mehmet Kanat <mehmet.kanat@protonmail.com>
2022-05-14 13:58:36 +0300

    Add dark defaultMode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,6 +38,7 @@ const config = {
   /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
         colorMode: {
+          defaultMode: 'dark',
           respectPrefersColorScheme: true,
           disableSwitch: true,
         },
